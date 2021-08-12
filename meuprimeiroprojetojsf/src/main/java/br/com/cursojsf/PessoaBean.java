529fe4cb81acf046d2529dc5c8015ded1f3440d3
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-08-12 00:56:23 -0300

    26.16 - Criando primeira página XHTML - JSF

--- meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
+++ meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
@@ -1,5 +1,25 @@
 package br.com.cursojsf;
 
-public class PessoaBean {
+import javax.faces.bean.ManagedBean;
 
+@ManagedBean(name = "pessoaBean")
+public class PessoaBean {
+	
+	private String nome;
+	private String sobrenome;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
 }
